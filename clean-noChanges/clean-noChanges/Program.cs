7f1323e9b7f507fc874ec12dfb55bb1f424d0036
oleksandrdubyna88 <oleksandrdubyna88@gmail.com>
2024-02-05 17:56:31 +0100

    fix bug with error message

---
+++ clean-noChanges/clean-noChanges/Program.cs
@@ -0,0 +1,35 @@
+using clean_noChanges.Client.Pages;
+using clean_noChanges.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(clean_noChanges.Client._Imports).Assembly);
+
+app.Run();
