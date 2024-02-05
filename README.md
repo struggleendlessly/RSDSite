c5e421dcadd3fe92eb8a3fe260fa34f6e570e5f9
oleksandrdubyna88 <oleksandrdubyna88@gmail.com>
2024-02-05 15:58:46 +0100

    add bootstrap theame

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # RSDSite
+
+https://themes.getbootstrap.com/preview/?theme_id=4231
+https://htmlstream.com/preview/front-v4.3.1/documentation/index.html
--- test/Program.cs
+++ test/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+static int[] reverse(int[] p)
+{
+
+    for (int i = 0; i <= p.Length/2; i++)
+    {
+        var f = p[i];
+        var l = p[p.Length-1-i];
+
+        p[i] = l; 
+        p[p.Length - 1 - i] = f;
+    }
+
+    return p;
+}
