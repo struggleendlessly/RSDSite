4f26e1588067528ba33e071f664a804931a23548
oleksandrdubyna88 <oleksandrdubyna88@gmail.com>
2024-02-05 16:46:58 +0100

    theme added to the home page

---
+++ shared/JSInvokeMethodList.cs
@@ -0,0 +1,21 @@
+﻿namespace shared
+{
+    public static class JSInvokeMethodList
+    {
+        public static string setRefferenceLayers = "js_setRefferenceLayers";
+        public static string setRefferenceKeyboardSvg = "js_setRefferenceKeyboardSvg";
+        public static string setRefferenceKeyboardButtons = "js_setRefferenceKeyboardButtons";
+
+        public static string onLoad = "js_onLoad";
+        public static string onLoadJsNavScroller = "js_onLoadJsNavScroller";
+        public static string onLoadJsNouislider = "js_onLoadNouislider";
+        public static string addButtons = "js_addButtons";
+        public static string setTitleShort = "js_setTitleShort";
+        public static string addMacrosBodyPart = "js_addMacrosBodyPart";
+        public static string downloadFileFromStream = "js_downloadFileFromStream";
+        public static string fillAvailableOptionsLayers = "js_fillAvailableOptionsLayers";
+        public static string cleanTextValuesForAllButtons = "js_cleanTextValuesForAllButtons";
+        public static string fillAvailableOptionsKeyboardButtons = "js_fillAvailableOptionsKeyboardButtons";
+        public static string scrollAvailableOptionsKeyboardButtons = "js_scrollAvailableOptionsKeyboardButtons";
+    }
+}
---
+++ web/web/Components/Pages/Home.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using shared;
+
+namespace web.Components.Pages
+{
+    public partial class Home : IDisposable
+    {
+        [Inject]
+        IJSRuntime JS { get; set; }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+
+
+            if (firstRender)
+            {
+                //objRef = DotNetObjectReference.Create(this); onLoadJsNavScroller
+                await JS.InvokeVoidAsync(JSInvokeMethodList.onLoad);
+            }
+        }
+    }
+}
