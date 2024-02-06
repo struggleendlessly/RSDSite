f0d058a4e1cf96d77a38f81a52883012ee87645e
oleksandrdubyna88 <oleksandrdubyna88@gmail.com>
2024-02-06 14:18:04 +0100

    edded rcl

---
+++ web/web/Components/Pages/PageWithScript.razor.js
@@ -0,0 +1,12 @@
+﻿export function onLoad() {
+    alert(123);
+    console.log('Loaded');
+}
+
+export function onUpdate() {
+    console.log('Updated');
+}
+
+export function onDispose() {
+    console.log('Disposed');
+}
