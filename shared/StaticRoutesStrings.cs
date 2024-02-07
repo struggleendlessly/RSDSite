29266c1eabe501a111640cefdc71f99cfb099755
Denys Porutskyi <excleake@gmail.com>
2024-02-07 19:44:12 +0200

    Changed the design of the login, registration and password reset pages

--- shared/StaticRoutesStrings.cs
+++ shared/StaticRoutesStrings.cs
@@ -8,5 +8,9 @@ namespace shared
         public const string AboutUsPageRoute = "/about-us";
         public const string ContactUsPageRoute = "/contact-us";
         public const string ServicesPageRoute = "/services";
+
+        public const string LoginPageRoute = "/Account/Login";
+        public const string ForgotPasswordPageRoute = "/Account/ForgotPassword";
+        public const string RegisterPageRoute = "/Account/Register";
     }
 }
