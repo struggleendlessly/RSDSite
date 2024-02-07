a863921274574f77fa870df9afdb13183a14dfd5
Denys Porutskyi <excleake@gmail.com>
2024-02-07 17:32:58 +0200

    Improved highlighting of active menu items

---
+++ shared/StaticHtmlStrings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace shared
+{
+    public static class StaticHtmlStrings
+    {
+        public static string ActiveMenuItemClass = "active";
+    }
+}
---
+++ shared/StaticRoutesStrings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace shared
+{
+    public static class StaticRoutesStrings
+    {
+        public const string MainPageRoute = "/";
+        public const string AboutUsPageRoute = "/about-us";
+        public const string ContactUsPageRoute = "/contact-us";
+        public const string ServicesPageRoute = "/services";
+    }
+}
