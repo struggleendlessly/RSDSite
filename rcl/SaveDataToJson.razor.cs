52550948ec3cf60dfb02e7b9e4bd6aed33406d67
oleksandrdubyna88 <oleksandrdubyna88@gmail.com>
2024-02-08 12:06:43 +0100

    save data to the json

---
+++ rcl/SaveDataToJson.razor.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace rcl
+{
+    public partial class SaveDataToJson
+    {
+        [Parameter]
+        public string JsonData { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            base.OnParametersSet();
+            // Add your code here
+            File.WriteAllText("data.json", JsonData);
+        }
+    }
+}
