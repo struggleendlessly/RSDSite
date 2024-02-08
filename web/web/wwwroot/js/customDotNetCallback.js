c3ed64e5c9aec47f7a4fd68456464af683088ad2
oleksandrdubyna88 <oleksandrdubyna88@gmail.com>
2024-02-08 12:23:03 +0100

    fix

--- web/web/wwwroot/js/customDotNetCallback.js
+++ web/web/wwwroot/js/customDotNetCallback.js
@@ -1,35 +1,4 @@
-﻿window.js_downloadFileFromStream = async (fileName, contentStreamReference) => {
-    const arrayBuffer = await contentStreamReference.arrayBuffer();
-    const blob = new Blob([arrayBuffer]);
-
-    const url = URL.createObjectURL(blob);
-
-    triggerFileDownload(fileName, url);
-
-    URL.revokeObjectURL(url);
-}
-
-function js_tinymceActivate() {
-    tinymce.init({
-        selector: 'textarea#tiny'
-    });
-}
-
-window.returnArrayAsync = (startPosition) => {
-    DotNet.invokeMethodAsync('web', 'ReturnArrayAsync', startPosition)
-        .then(data => {
-            console.log(data);
-        });
-};
-
-window.returnTinyMceContent = (content) => {
-    DotNet.invokeMethodAsync('web', 'returnTinyMceContent', content)
-        .then(data => {
-            console.log(data);
-        });
-};
-
-class DotNetHelpers {
+﻿class DotNetHelpers {
     static dotNetHelper;
 
     static setDotNetHelper(value) {
