1c293b0b2a1a335733cdef4bad3690fcdaa822ef
Denys Porutskyi <excleake@gmail.com>
2024-02-15 19:07:09 +0200

    Refactoring of the TinyMceEditor component

---
+++ web/web/Components/Shared/TinyMceEditor.razor.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+using shared.Interfaces;
+using shared;
+
+namespace web.Components.Shared
+{
+    public partial class TinyMceEditor
+    {
+        [Parameter]
+        public string TinyMceId { get; set; } = string.Empty;
+
+        [Parameter]
+        public ITinyMceEditable Parent { get; set; }
+
+        [Parameter]
+        public string Key { get; set; } = string.Empty;
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        private bool EditMode { get; set; } = false;
+
+        private string Value { get; set; } = string.Empty;
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            Value = Parent.Model.Data[Key];
+        }
+
+        private async Task ToggleEditModeAsync()
+        {
+            EditMode = !EditMode;
+
+            if (EditMode)
+            {
+                await InitializeTinyMCEAsync();
+            }
+            else
+            {
+                await DestroyTinyMCEAsync();
+            }
+        }
+
+        private async Task InitializeTinyMCEAsync()
+        {
+            await JSRuntime.InvokeVoidAsync(JSInvokeMethodList.tinymceActivate, TinyMceId);
+        }
+
+        private async Task DestroyTinyMCEAsync()
+        {
+            await JSRuntime.InvokeVoidAsync(JSInvokeMethodList.tinymceDestroy, TinyMceId);
+        }
+
+        private async Task SaveChangesAsync()
+        {
+            var content = await JSRuntime.InvokeAsync<string>(JSInvokeMethodList.tinymceGetContent, TinyMceId);
+
+            Parent.Model.Data[Key] = content;
+            Parent.Save();
+
+            await ToggleEditModeAsync();
+        }
+
+        private async Task CancelEditAsync()
+        {
+            await ToggleEditModeAsync();
+        }
+    }
+}
