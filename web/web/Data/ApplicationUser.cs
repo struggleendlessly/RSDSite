cd962b10777a43d05eccf1e1689be9b550788603
Denys Porutskyi <excleake@gmail.com>
2024-02-28 22:47:49 +0200

    Added a field SiteName for registration

--- web/web/Data/ApplicationUser.cs
+++ web/web/Data/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace web.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public string SiteName { get; set; }
     }
 
 }
--- web/web/Data/Migrations/20240228203633_AddSiteNameFieldToTheApplicationUserEntity.Designer.cs
+++ web/web/Data/Migrations/20240228203633_AddSiteNameFieldToTheApplicationUserEntity.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using web.Data;
 
 #nullable disable
 
-namespace web.Migrations
+namespace web.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240228203633_AddSiteNameFieldToTheApplicationUserEntity")]
+    partial class AddSiteNameFieldToTheApplicationUserEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
