0a7621601216f3915f76f575f49cb56719fe7918
Denys Porutskyi <excleake@gmail.com>
2024-03-22 17:43:28 +0200

    Improvements in the Logout component

--- web/web/Components/Account/Pages/Logout.razor.cs
+++ web/web/Components/Account/Pages/Logout.razor.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 
 using shared;
 using web.Data;
-using Microsoft.AspNetCore.Components.Authorization;
-using Microsoft.JSInterop;
-using System.Security.Claims;
 
 namespace web.Components.Account.Pages
 {
@@ -20,17 +18,16 @@ namespace web.Components.Account.Pages
         [Inject]
         NavigationManager NavigationManager { get; set; }
 
-        ClaimsPrincipal User { get; set; }
-
         protected override async Task OnInitializedAsync()
         {
             var authstate = await AuthenticationStateProvider.GetAuthenticationStateAsync();
-            User = authstate.User;
-
-            if (SignInManager.IsSignedIn(User))
+            if (SignInManager.IsSignedIn(authstate.User))
             {
-                await SignInManager.SignOutAsync();
-                NavigationManager.NavigateTo(StaticRoutesStrings.LogoutPageRoute, forceLoad: true);
+                await SignInManager.SignOutAsync();           
+            }
+            else
+            {
+                NavigationManager.NavigateTo(StaticRoutesStrings.MainPageRoute);
             }
         }
     }
