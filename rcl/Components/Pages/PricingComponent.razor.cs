bcbb4f4d4314153d21d6aaf0824c79a3be77a937
oleksandrdubyna88 <oleksandrdubyna88@gmail.com>
2024-04-22 00:36:17 +0200

    new design

---
+++ rcl/Components/Pages/PricingComponent.razor.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Options;
+
+using shared.ConfigurationOptions;
+
+namespace rcl.Components.Pages
+{
+    public partial class PricingComponent
+    {
+        [Parameter]
+        public string SiteName { get; set; }
+
+        [Parameter]
+        public string Lang { get; set; }
+
+        [Inject]
+        public IOptions<StripeOptions> stripeOptions { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+
+        }
+    }
+}
