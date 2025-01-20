2ef9abe95e5170b9b76eec439f23a1c9d2e02a5f
Denys Porutskyi <excleake@gmail.com>
2025-01-20 20:47:07 +0200

    Added support for Service, Documents and Portfolio pages in wasm

---
+++ webassembly/Pages/Documents.razor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace webassembly.Pages
+{
+    public partial class Documents
+    {
+        [Parameter]
+        public string? SiteName { get; set; } = null;
+
+        [Parameter]
+        public string Lang { get; set; } = string.Empty;
+    }
+}
